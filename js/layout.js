91d8e952b943dab10d863add471334aee10ecabf
LeeBosa <grandsky95@hotmail.com>
2023-01-31 10:46:05 +0900

    23.01.31

--- js/layout.js
+++ js/layout.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     // 페이지 설정
         // 페이지 분류
         const index = $(".index_wrap"); // 메인 페이지
-        const career = $(".career_wrap"); // 커리어 페이지
+        const aboutme = $(".aboutme_wrap"); // 소개 페이지
         const portfolio = $(".portfolio_wrap"); // 포트폴리오 페이지
         const skill = $(".skill_wrap"); // 스킬 페이지
 
@@ -30,7 +30,7 @@ $(document).ready(function(){
                     +'</ul>'
                     +'<a href="/vision/index.html"><h1><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 264.96 179.23"><defs><style></style></defs><path class="logo" d="M262.38,.42c-69.12-.72-49.53-5.77-80.64,52.58,.91,.57,1.83,1.14,2.74,1.71,11.34-13.75,20.13-31.89,33.32-43.38,6.4-.72,12.93-.26,18.13-.26-35.46,35.47-70.95,70.96-107.14,107.15-13.41-15.38-32.46-29.28-41.05-48.91-10.12-21.14-20.78-42.02-31.43-62.9-1.18-2.32-3.65-5.62-5.59-5.67C33.52,.29,16.3,.48,0,.48,44.08,44.54,88.14,88.6,133.31,133.75c4.09-4.69,8.95-10.27,13.81-15.85,.53,.37,1.06,.74,1.59,1.11-5.78,11.56-11.57,23.12-17.13,34.24-9.76-11.82-18.8-22.78-27.85-33.73-.75,.5-1.49,1.01-2.24,1.51,9.49,18.97,18.97,37.93,29.11,58.2,18.01-31.29,29.22-65.78,50.36-94.83,28.43-26.01,52.67-58.44,84-80.43-.86-1.19-1.72-2.37-2.57-3.56ZM23.67,11.08c8.19,1.31,18.91-3.45,24.35,3.34,6.86,13.37,13.54,26.84,21.31,42.36-15.9-15.92-30.3-30.32-45.66-45.7Z"/></svg></h1></a>'
                     +'<ul class="gnb gnb_right">'
-                        +'<li><a class="gnb_menu" href="/vision/sub/career.html">커리어</a></li>'
+                        +'<li><a class="gnb_menu" href="/vision/sub/aboutme.html">소개</a></li>'
                         +'<li><a class="gnb_menu" href="/vision/sub/contacts.html">연락처</a></li>'
                     +'</ul>'
                 +'</div>'
@@ -53,7 +53,7 @@ $(document).ready(function(){
                                 +'<span></span>'
                             +'</div>'
                             +'<div class="header_menu">'
-                                +'<h2><a href="/vision/sub/career.html">커리어</a></h2>'
+                                +'<h2><a href="/vision/sub/aboutme.html">소개</a></h2>'
                                 +'<span></span>'
                             +'</div>'
                             +'<div class="header_menu">'
@@ -206,7 +206,7 @@ $(document).ready(function(){
             });
         }
         // 커리어 페이지
-        else if(url.includes("career.html")) {
+        else if(url.includes("aboutme.html")) {
             $(window).resize(function(){ // 윈도우 리사이징
                 scrollAnimCheck();
                 return;
@@ -232,36 +232,35 @@ $(document).ready(function(){
                 scrollTop = $(window).scrollTop();
                 scrollBottom = scrollTop + window.innerHeight;
                 profileTop = $(".profile").offset().top;
-                careerTop = $(".career").offset().top;
 
                 if(window.innerWidth > 1080) { // PC에서
                     if(scrollTop <= 0) { // 맨위
-                        career.find(".scroll_animation").removeClass("hide");
-                        career.find(".profile").removeClass("show");
+                        aboutme.find(".scroll_animation").removeClass("hide");
+                        aboutme.find(".profile").removeClass("show");
                     }
                     else { // 맨위가 아닐 때
-                        career.find(".scroll_animation").addClass("hide");
-                        career.find(".profile").addClass("show");
+                        aboutme.find(".scroll_animation").addClass("hide");
+                        aboutme.find(".profile").addClass("show");
                     }
         
                     if(profileTop > scrollTop) { // 프로필이 화면보다 위에 있을 때
-                        career.find(".profile_img_wrap")
+                        aboutme.find(".profile_img_wrap")
                         .removeClass("fixed")
                         .removeClass("bottom");
                     }
-                    else if(profileTop <= scrollTop && careerTop > scrollBottom) { // 프로필이 화면보다 아래고, 커리어보다 위에 있을 때
-                        career.find(".profile_img_wrap")
+                    else if(profileTop <= scrollTop) { // 프로필이 화면보다 아래에 있을 때
+                        aboutme.find(".profile_img_wrap")
                         .removeClass("bottom")
                         .addClass("fixed");
                     }
                     else { // 그 외에
-                        career.find(".profile_img_wrap")
+                        aboutme.find(".profile_img_wrap")
                         .removeClass("fixed")
                         .addClass("bottom");
                     }
                 }
                 else { // PC 이외에
-                    career.find(".profile_img_wrap")
+                    aboutme.find(".profile_img_wrap")
                     .removeClass("fixed")
                     .removeClass("bottom");
                 }
