06d43d322995b62b182ecaee97c090e2fb6e67af
taeholee235 <taedonn@outlook.com>
2023-05-02 10:06:30 +0900

    Change nav link

--- src/router/router.js
+++ src/router/router.js
@@ -5,8 +5,8 @@ import ContactsPage from "@/components/ContactsPage";
 
 const routes = [ // path별 component를 추가한다.
     { path : "/", name : "Home", component : MainPage },
-    { path : "/main/web", name : "Web", component : MainPage },
-    { path : "/main/product", name : "Product", component : ProductPage },
+    { path : "/web", name : "Web", component : MainPage },
+    { path : "/product", name : "Product", component : ProductPage },
     { path : "/contacts", name : "Contacts", component : ContactsPage },
     { path : "/:catchAll(.*)", name : "Home", component : MainPage }, // :catchAll(.*)은 선언한 path 이외의 path는 전부 MainPage로 보내겠다고 선언 (주로 Error Page로 선언)
 ]
