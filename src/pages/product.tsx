69eb8b169f36ab427f745edec8c206158a5cb269
taeholee235 <taedonn@outlook.com>
2023-06-19 12:58:18 +0900

    Update favicon

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -16,84 +16,84 @@ export default function Product () {
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">UD CHAIR V2</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">DIY Furniture Project - Chair</p>
                     </div>
-                    <Image src="/product-ud-chair-2.png" alt="UD 의자 2" width="400" height="400" className="h-[80%] object-contain"/>
+                    <Image src="/product-ud-chair-2.png" alt="UD 의자 2" width="280" height="280" className="h-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">UD CHAIR V1</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">DIY Furniture Project - Chair</p>
                     </div>
-                    <Image src="/product-ud-chair-1.png" alt="UD 의자 1" width="400" height="400" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-ud-chair-1.png" alt="UD 의자 1" width="280" height="280" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">STACK UP - TABLE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">DIY Furniture Project - Table</p>
                     </div>
-                    <Image src="/product-stack-table.png" alt="스택업 - 책상" width="400" height="400" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-stack-table.png" alt="스택업 - 책상" width="280" height="280" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">STACK UP - STOOL</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">DIY Furniture Project - Stool</p>
                     </div>
-                    <Image src="/product-stack-stool.png" alt="스택업 - 의자" width="400" height="400" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-stack-stool.png" alt="스택업 - 의자" width="280" height="280" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">SLIDE&#39;M</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">DIY Furniture Project - Desk</p>
                     </div>
-                    <Image src="/product-slide-m.png" alt="슬라이뎀" width="400" height="400" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-slide-m.png" alt="슬라이뎀" width="280" height="280" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">PATH</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">DIY Furniture Project - Shelf</p>
                     </div>
-                    <Image src="/product-shelf.png" alt="패스" width="400" height="400" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-shelf.png" alt="패스" width="280" height="280" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">CUSTABLE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">DIY Furniture Project - Table</p>
                     </div>
-                    <Image src="/product-custable.png" alt="커스터블" width="400" height="400" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-custable.png" alt="커스터블" width="280" height="280" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">FORMATT</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">DIY Furniture Project - Mattress</p>
                     </div>
-                    <Image src="/product-formatt.png" alt="포멧" width="400" height="400" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-formatt.png" alt="포멧" width="280" height="280" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">COSMO</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent text-center font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">Johnson Controls&#39; Smart Home Assistant</p>
                     </div>
-                    <Image src="/product-cosmo.png" alt="코스모" width="400" height="400" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-cosmo.png" alt="코스모" width="280" height="280" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent text-center font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">CARDBOARD LIGHT</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">Cardboard Upcycling Project V3</p>
                     </div>
-                    <Image src="/product-cardboard-light.png" width="400" height="400" alt="카드보드 샹들리에" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-cardboard-light.png" width="280" height="280" alt="카드보드 샹들리에" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent text-center font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">CARDBOARD TABLE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">Cardboard Upcycling Project V2</p>
                     </div>
-                    <Image src="/product-cardboard-table.png" width="400" height="400" alt="카드보드 책상" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-cardboard-table.png" width="280" height="280" alt="카드보드 책상" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group cursor-pointer">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent text-center font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">CARDBOARD CHAIR</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">Cardboard Upcycling Project V1</p>
                     </div>
-                    <Image src="/product-cardboard-chair.png" width="400" height="400" alt="카드보드 의자" className="h-[80%] max-w-[80%] object-contain"/>
+                    <Image src="/product-cardboard-chair.png" width="280" height="280" alt="카드보드 의자" className="h-[80%] max-w-[80%] object-contain"/>
                 </div>
                 <div className="w-[calc(25%-12px)]"></div>
                 <div className="w-[calc(25%-12px)]"></div>
--- src/pages/web-dev.tsx
+++ src/pages/web-dev.tsx
@@ -17,56 +17,56 @@ export default function WebDev () {
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">FONTS ARCHIVE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">https://fonts.taedonn.com</p>
                     </div>
-                    <Image src="/web-fonts-archive.png" alt="상업용 무료 한글 폰트 아카이빙 웹사이트" width="400" height="400" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
+                    <Image src="/web-fonts-archive.png" alt="상업용 무료 한글 폰트 아카이빙 웹사이트" width="280" height="280" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
                 </Link>
                 <Link href="https://gdrive-img-converter.taedonn.com" target="_blank" className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">GDRIVE IMAGE CONVERTER</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">https://gdrive-img-converter.taedonn.com</p>
                     </div>
-                    <Image src="/web-google-drive-image-converter.png" alt="구글 드라이브 이미지 변환기" width="400" height="400" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
+                    <Image src="/web-google-drive-image-converter.png" alt="구글 드라이브 이미지 변환기" width="280" height="280" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
                 </Link>
                 <Link href="https://gitbook.taedonn.com" target="_blank" className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">GITBOOK TEMPLATE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">https://gitbook.taedonn.com</p>
                     </div>
-                    <Image src="/web-gitbook-theme.png" alt="깃북 템플릿" width="400" height="400" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
+                    <Image src="/web-gitbook-theme.png" alt="깃북 템플릿" width="280" height="280" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
                 </Link>
                 <Link href="https://resume.taedonn.com" target="_blank" className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">WEB RESUME</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">https://resume.taedonn.com</p>
                     </div>
-                    <Image src="/web-resume.png" alt="웹 이력서" width="400" height="400" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
+                    <Image src="/web-resume.png" alt="웹 이력서" width="280" height="280" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
                 </Link>
                 <Link href="https://clone-robomd.taedonn.com" target="_blank" className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">ROBOMD CLONE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">https://clone-robomd.taedonn.com</p>
                     </div>
-                    <Image src="/web-robomd.png" alt="RoboMD 클론 코딩" width="400" height="400" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
+                    <Image src="/web-robomd.png" alt="RoboMD 클론 코딩" width="280" height="280" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
                 </Link>
                 <Link href="https://clone-astellnkern.taedonn.com" target="_blank" className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">ASTELL&KERN CLONE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">https://clone-astellnkern.taedonn.com</p>
                     </div>
-                    <Image src="/web-astellnkern.png" alt="Astell&Kern 클론 코딩" width="400" height="400" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
+                    <Image src="/web-astellnkern.png" alt="Astell&Kern 클론 코딩" width="280" height="280" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
                 </Link>
                 <Link href="https://clone-solaseado.taedonn.com" target="_blank" className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">SOLASEADO CLONE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">https://clone-solaseado.taedonn.com</p>
                     </div>
-                    <Image src="/web-solaseado.png" alt="솔라시도 클론 코딩" width="400" height="400" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
+                    <Image src="/web-solaseado.png" alt="솔라시도 클론 코딩" width="280" height="280" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
                 </Link>
                 <Link href="https://clone-jejuair.taedonn.com" target="_blank" className="portfolio-box w-[calc(25%-12px)] tlg:w-[calc(50%-8px)] tsm:w-[100%] h-[16vw] tlg:h-[28vw] tsm:h-[50vw] relative flex flex-row justify-center items-center bg-theme-white-2 mt-[16px] tsm:mt-[12px] group">
                     <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex flex-col justify-center items-center bg-theme-white-2/0 group-hover:bg-theme-white-2/80 tlg:group-hover:bg-transparent duration-300">
                         <p className="text-[1.04vw] text-theme-dark-2/0 group-hover:text-theme-dark-2 tlg:group-hover:text-transparent font-extrabold tracking-widest translate-y-[1vw] group-hover:translate-y-0 duration-200">JEJU AIR CLONE</p>
                         <p className="text-[0.83vw] text-theme-dark-2/0 group-hover:text-theme-dark-2/60 tlg:group-hover:text-transparent font-normal tracking-tight mt-[0.21vw] translate-y-[1vw] group-hover:translate-y-0 duration-200 delay-100">https://clone-jejuair.taedonn.com</p>
                     </div>
-                    <Image src="/web-jejuair.png" alt="제주항공 클론 코딩" width="400" height="400" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
+                    <Image src="/web-jejuair.png" alt="제주항공 클론 코딩" width="280" height="280" className="h-[95%] object-contain translate-x-[0.5vw] translate-y-[1vw] tlg:translate-y-[2vw] tsm:translate-y-[3.5vw]"/>
                 </Link>
                 <div className="w-[calc(25%-12px)]"></div>
                 <div className="w-[calc(25%-12px)]"></div>
