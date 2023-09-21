31f28e247d030e0e49a5da2dd61f47f8e5b060b3
taedonn <taedonn@taedonn.com>
2023-09-21 22:47:16 +0900

    changed header height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header ({theme}:{theme:string}) {
     return (
         <>
-            <header style={theme === "dark" ? {backgroundColor: "#000000CC"} : {backgroundColor: "#FFFFFFCC"}} className="w-[100%] h-[120px] tlg:h-[100px] tsm:h-[80px] fixed left-0 top-0 z-20 flex flex-row justify-between items-center px-[48px] tlg:px-[36px] tsm:px-[24px] backdrop-blur">
+            <header style={theme === "dark" ? {backgroundColor: "#000000CC"} : {backgroundColor: "#FFFFFFCC"}} className="w-[100%] h-[100px] tlg:h-[80px] fixed left-0 top-0 z-20 flex flex-row justify-between items-center px-[48px] tlg:px-[36px] tsm:px-[24px] backdrop-blur">
                 <a href="/"><h1 style={theme === "dark" ? {color: "#FFFFFF"} : {color: "#202124"}} className="block p-[12px] tsm:p-0 text-[13px] font-semibold">T A E D O N N</h1></a>
                 <ul className="flex flex-row justify-end items-center">
                     <a href="/web-dev" style={theme === "dark" ? {color: "#FFFFFF"} : {color: "#202124"}} className="block p-[12px] tsm:p-0 font-semibold text-[11px] text-end tracking-widest">
