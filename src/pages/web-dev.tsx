8d1ba58bd24588a5081d160e22d3c14d33d9221c
taeho <taedonn@taedonn.com>
2023-09-25 15:08:13 +0900

    change anchor target

--- src/pages/web-dev.tsx
+++ src/pages/web-dev.tsx
@@ -40,7 +40,7 @@ const WebDev = ({params}: any) => {
                                             }
                                         </div>
                                         <div className="flex items-center absolute left-[1.46vw] txl:left-[2.19vw] tlg:left-[2.74vw] tsm:left-[4.38vw] bottom-[1.46vw] txl:bottom-[2.19vw] tlg:bottom-[2.74vw] tsm:bottom-[4.38vw] text-theme-blue text-[0.83vw] txl:text-[1.25vw] tlg:text-[1.56vw] tsm:text-[2.49vw] mt-[1.46vw] txl:mt-[2.19vw] tlg:mt-[2.74vw] tsm:mt-[4.38vw] cursor-pointer">
-                                            <a href={dataMap.url} className="group flex items-center mr-[0.83vw] txl:mr-[1.25vw] tlg:mr-[1.56vw] tsm:mr-[2.5vw]">
+                                            <a href={dataMap.url} target="_blank" className="group flex items-center mr-[0.83vw] txl:mr-[1.25vw] tlg:mr-[1.56vw] tsm:mr-[2.5vw]">
                                                 <svg className="w-[0.83vw] txl:w-[1.17vw] tlg:w-[1.46vw] tsm:w-[2.34vw] mr-[0.1vw] txl:mr-[0.16vw] tlg:mr-[0.2vw] tsm:mr-[0.31vw] group-hover:rotate-[45deg] tlg:group-hover:rotate-0 group-hover:translate-x-[-0.21vw] tlg:group-hover:translate-x-0 duration-200 tlg:duration-0 fill-theme-blue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                                                     <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
                                                 </svg>
@@ -51,7 +51,7 @@ const WebDev = ({params}: any) => {
                                             </a>
                                             {
                                                 dataMap.github_link_yn
-                                                ? <a href={dataMap.github_link} className="group flex items-center">
+                                                ? <a href={dataMap.github_link} target="_blank" className="group flex items-center">
                                                     <svg className="w-[0.83vw] txl:w-[1.17vw] tlg:w-[1.46vw] tsm:w-[2.34vw] mr-[0.1vw] txl:mr-[0.16vw] tlg:mr-[0.2vw] tsm:mr-[0.31vw] group-hover:rotate-[45deg] tlg:group-hover:rotate-0 group-hover:translate-x-[-0.21vw] tlg:group-hover:translate-x-0 duration-200 tlg:duration-0 fill-theme-blue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                                                         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                                                     </svg>
