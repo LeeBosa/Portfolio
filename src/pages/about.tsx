c2e40889ddf14b1035d0b5cc63a4c146c4e7a538
taeho <taedonn@taedonn.com>
2023-12-19 09:59:23 +0900

    Update date

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,12 @@
 // 훅
-import Link from "next/link";
 import { NextSeo } from "next-seo";
 
 // 컴포넌트
 import Header from "@/components/Header";
 
 export default function About () {
+    const currYear = new Date().getFullYear();
+
     return (
         <>
             <NextSeo title={"T A E D O N N · 연락처"}/>
@@ -18,7 +19,7 @@ export default function About () {
                     <h2 className="text-[20px] text-center text-theme-1 font-semibold tracking-widest mt-[60px]">TAEHO LEE&#40;TAEDONN&#41;</h2>
                     <div className="text-[14px] text-theme-1 font-light tracking-wide mt-[4px]">taedonn@taedonn.com</div>
                     <div className="flex flex-row justify-center items-center mt-[60px]">
-                        <h3 className="w-[80px] text-[12px] text-theme-1 tracking-wider font-light">2021~2023</h3>
+                        <h3 className="w-[80px] text-[12px] text-theme-1 tracking-wider font-light">2021~{currYear}</h3>
                         <h3 className="text-[12px] text-theme-1 tracking-wider font-light">iCLAVE</h3>
                     </div>
                 </div>
